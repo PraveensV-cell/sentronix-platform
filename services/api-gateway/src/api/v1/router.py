bfb7cb9addd4a0fe1474095113d10e36a6dc08f9
Shidigonde Praveen <shidigondePraveenshetty@gmail.com>
2026-07-21 23:24:08 +0530

    refactor(api-gateway): introduce enterprise architecture

---
+++ services/api-gateway/src/api/v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+api_router = APIRouter()
+
+# Future
+# api_router.include_router(auth.router)
+# api_router.include_router(camera.router)
+# api_router.include_router(ai.router)
